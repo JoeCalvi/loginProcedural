23569c56a54b5a0788430eade2630d7ec463c1a2
JoeCalvi <joecalvi@me.com>
2023-07-02 16:29:31 -0600

    added bootstrap, some styling

--- header.php
+++ header.php
@@ -8,40 +8,58 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title></title>
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+  <link rel="stylesheet" href="./style.css">
 </head>
 <body>
 
 
   <header>
-      <nav class="navbar">
-        <ul>
-          <li><a href="index.php">Home</a></li>
-          <li><a href="#">Portfolio</a></li>
-          <li><a href="#">About</a></li>
-          <li><a href="#">Contact</a></li>
-        </ul>
-        <div>
-
-        <?php
-
-            if(isset($_SESSION['userId'])) {
-
-              echo '<form action="includes/logout.inc.php" method="post">
-              <button type="submit" name="logout-submit">Logout</button>
-            </form>';
-
-            } else {
-
-              echo '<form action="includes/login.inc.php" method="post">
-              <input type="text" name="mailuid" placeholder="Username/E-mail...">
-              <input type="password" name="pwd" placeholder="Password...">
-              <button type="submit" name="login-submit">Login</button>
-            </form>
-            <a href="signup.php">Signup</a>';
-
-            }
-            
-          ?>
+      <nav class="navbar bg-dark text-light h-100">
+        <div class="container-fluid">
+          <div class="d-flex gap-4 me-auto mb-2">
+            <div class="nav-item">
+              <a class="nav-link" href="index.php">Home</a>
+            </div>
+            <div class="nav-item">
+              <a class="nav-link" href="#">Portfolio</a>
+            </div>
+            <div class="nav-item">
+              <a class="nav-link" href="#">About</a>
+            </div>
+            <div class="nav-item">
+              <a class="nav-link" href="#">Contact</a>
+            </div>
+          </div>
+          <div>
+  
+          <?php
+  
+              if(isset($_SESSION['userId'])) {
+  
+                echo '
+                <form class="d-flex" action="includes/logout.inc.php" method="post">
+                  <button class="btn btn-outline-danger" type="submit" name="logout-submit">Logout</button>
+                </form>';
+  
+              } else {
+  
+                echo '
+                <div class="d-flex align-items-center">
+                  <form class="d-flex gap-2 pe-2" action="includes/login.inc.php" method="post">
+                    <input class="form-control" type="text" name="mailuid" placeholder="Username/E-mail...">
+                    <input class="form-control" type="password" name="pwd" placeholder="Password...">
+                    <button class="btn btn-outline-primary" type="submit" name="login-submit">Login</button>
+                  </form>
+                  <button class="btn btn-outline-light">
+                  <a class="nav-link" href="signup.php">Signup</a>
+                  </button>
+                </div>';
+  
+              }
+              
+            ?>
+          </div>
         </div>
       </nav>
   </header>
--- signup.php
+++ signup.php
@@ -3,36 +3,50 @@
 ?>
 
     <main>
-      <div>
-        <section>
-          <h1>Signup</h1>
-          <?php
-            if(isset($_GET['error'])) {
-              if($_GET['error'] == "emptyfields") {
-                echo '<p>Fill in all fields.</p>';
-              } else if($_GET['error'] == "invaliduidmailuid") {
-                echo '<p>Invalid username or email.</p>';
-              } else if($_GET['error'] == "invaliduidmail") {
-                echo '<p>Invalid email.</p>';
-              } else if($_GET['error'] == "invaliduid") {
-                echo '<p>Invalid username.</p>';
-              } else if($_GET['error'] == "passwordcheck") {
-                echo '<p>Passwords do not match.</p>';
-              } else if($_GET['error'] == "usertaken") {
-                echo '<p>Username already taken.</p>';
-              } else if($_GET['signup'] == "success") {
-                echo '<p>Signup successful!</p>';
-              }
-            }
-          ?>
-          <form action="includes/signup.inc.php" method="post">
-            <input type="text" name="uid" placeholder="Username">
-            <input type="text" name="mail" placeholder="E-mail">
-            <input type="password" name="pwd" placeholder="Password">
-            <input type="password" name="pwd-repeat" placeholder="Repeat password">
-            <button type="submit" name="signup-submit">Signup</button>
-          </form>
-        </section>
+      <div class="container-fluid h-100">
+        <div class="row justify-content-center align-items-center h-75">
+          <div class="col-6 d-flex flex-column signup justify-content-evenly">
+            <div class="text-center">
+              <h1>Signup</h1>
+            </div>
+              <?php
+                if(isset($_GET['error'])) {
+                  if($_GET['error'] == "emptyfields") {
+                    echo '<p>Fill in all fields.</p>';
+                  } else if($_GET['error'] == "invaliduidmailuid") {
+                    echo '<p>Invalid username or email.</p>';
+                  } else if($_GET['error'] == "invaliduidmail") {
+                    echo '<p>Invalid email.</p>';
+                  } else if($_GET['error'] == "invaliduid") {
+                    echo '<p>Invalid username.</p>';
+                  } else if($_GET['error'] == "passwordcheck") {
+                    echo '<p>Passwords do not match.</p>';
+                  } else if($_GET['error'] == "usertaken") {
+                    echo '<p>Username already taken.</p>';
+                  } else if($_GET['signup'] == "success") {
+                    echo '<p>Signup successful!</p>';
+                  }
+                }
+              ?>
+              <form action="includes/signup.inc.php" method="post">
+                <div class="mb-3">
+                  <input class="form-control" type="text" name="uid" placeholder="Username">
+                </div>
+                <div class="mb-3">
+                  <input class="form-control" type="text" name="mail" placeholder="E-mail">
+                </div>
+                <div class="mb-3">
+                  <input class="form-control" type="password" name="pwd" placeholder="Password">
+                </div>
+                <div class="mb-3">
+                  <input class="form-control" type="password" name="pwd-repeat" placeholder="Repeat password">
+                </div>
+                <div class="w-100 d-flex justify-content-end">
+                  <button class="btn btn-outline-success" type="submit" name="signup-submit">Signup</button>
+                </div>
+              </form>
+          </div>
+        </div>
       </div>
     </main>
 
