d47f9c070fbbe37f6ed4b9c4292112439a710344
JoeCalvi <joecalvi@me.com>
2023-07-02 12:50:17 -0600

    setups

--- header.php
+++ header.php
@@ -6,6 +6,27 @@
   <title></title>
 </head>
 <body>
+
+
   <header>
-    
+      <nav class="navbar">
+        <ul>
+          <li><a href="index.php">Home</a></li>
+          <li><a href="#">Portfolio</a></li>
+          <li><a href="#">About</a></li>
+          <li><a href="#">Contact</a></li>
+        </ul>
+        <div>
+          <!-- Always use "post" method for sensitive data -->
+          <form action="includes/login.inc.php" method="post">
+            <input type="text" name="mailuid" placeholder="Username/E-mail...">
+            <input type="password" name="pwd" placeholder="Password...">
+            <button type="submit" name="login-submit">Login</button>
+          </form>
+          <a href="signup.php">Signup</a>
+          <form action="includes/logout.inc.php" method="post">
+            <button type="submit" name="logout-submit">Logout</button>
+          </form>
+        </div>
+      </nav>
   </header>
